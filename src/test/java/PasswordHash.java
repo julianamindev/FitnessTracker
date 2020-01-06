c4cef2d60943e341b8cbbdd28f1d5d8aedffbe5c
Julian Amin <jamin@gofluent.com>
2020-01-06 10:59:25 +0000

    added test for bcrypt hash

--- src/test/java/PasswordHash.java
+++ src/test/java/PasswordHash.java
@@ -1,7 +1,15 @@
 import junit.framework.TestCase;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordHash extends TestCase {
+
+    public void testBCryptHash() {
+        String password = "Pupstamesa2013";
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode(password);
+        System.out.println(hashedPassword);
+    }
 
     public void testMD5Hash() {
         String password = "Pupstamesa2013";
