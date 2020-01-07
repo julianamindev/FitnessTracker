a12eba49acdb02b487d322bfde390e7785138ea4
Julian Amin <jamin@gofluent.com>
2020-01-07 07:35:37 +0000

    updated controller for 403 functionality

--- src/main/java/com/pluralsight/controller/LoginController.java
+++ src/main/java/com/pluralsight/controller/LoginController.java
@@ -25,4 +25,9 @@ public class LoginController {
     public String logout(ModelMap model) {
         return "logout";
     }
+
+    @RequestMapping(value = "/403", method = RequestMethod.GET)
+    public String error403(ModelMap model) {
+        return "403";
+    }
 }
---
+++ src/main/webapp/WEB-INF/jsp/403.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Access Denied</title>
+</head>
+<body>
+    <h1>You are not authorized!</h1>
+</body>
+</html>
